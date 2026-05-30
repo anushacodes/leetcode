1b3b48eeb07de13b3168aaf88f6bd6ace8658fa1
Anusha <66325804+anushacodes@users.noreply.github.com>
2026-05-30 17:25:09 -0500

    14 ms (90.92%) 10.7 MB (78.92%) Easy

---
+++ LeetCode/Easy/0217-contains-duplicate/0217-contains-duplicate.go
@@ -0,0 +1,10 @@
+func containsDuplicate(nums []int) bool {
+    slices.Sort(nums)
+
+    for i := 1; i < len(nums); i++ {
+        if nums[i] == nums[i-1]{
+            return true
+        } 
+    }
+    return false
+}
