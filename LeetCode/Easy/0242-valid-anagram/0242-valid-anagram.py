97fba8edda8cce9aec503f6e4ffe71561eecc19c
Anusha <66325804+anushacodes@users.noreply.github.com>
2026-05-30 17:48:05 -0500

    9 ms (79.81%) 19.5 MB (43.89%) Easy

--- LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.py
+++ LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.py
@@ -1,7 +1,30 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if sorted(t) == sorted(s):
-            return True
-        else:
+        if len(s) != len(t):
             return False
+
+        else:
+            # create a dict of all letters here
+            # then add frequencies
+            # then comapre dicts?
+            # return sorted(s) == sorted(t)
+
+            sc = {}
+            tc = {}
+
+            for l in s:
+                sc[l] = sc.get(l, 0) + 1
+                # print("s: ", sc)
+
+            for l in t:
+                tc[l] = tc.get(l, 0) + 1
+                # print("t: ", tc)
+
+            return sc == tc
+
+
+            # optim: only 1 dict, do c - 1
+
+            
+
         
