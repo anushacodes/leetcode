b6d0134e3a5648e028ae338413ea8f6e7907ecb4
Anusha <66325804+anushacodes@users.noreply.github.com>
2026-07-02 13:32:29 -0500

    299 ms (88.13%) 19.4 MB (21.25%) Medium

---
+++ 0018-4sum/0018-4sum-07-02-2026-13-32-29.py
@@ -0,0 +1,36 @@
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+        ans, li = [], []
+        # fix 2 nums, subtract from target then 2sum to find other two
+        for i in range(len(nums)):
+            # skip if repeat
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            n1 = nums[i]
+            for j in range(i+1, len(nums)): 
+                # skip curr(j) if same as prev
+                if j > i + 1 and nums[j - 1] == nums[j]:
+                    continue              
+                n2 = nums[j]
+                rem = target - (n1 + n2)
+                # two pointer starts here
+                l, r = j + 1, len(nums) - 1
+                while l < r:
+                    s = nums[l] + nums[r]
+                    if s == rem:
+                        ans.append([n1, n2, nums[l], nums[r]])
+                        l += 1
+                        r -= 1
+                        while (l < r and nums[l] == nums[l - 1]):
+                            l += 1
+                    if s > rem: 
+                        r -= 1
+                    elif s < rem:
+                        l += 1
+
+        return ans
+        
+
+    
+        
