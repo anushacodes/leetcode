f0e8920b1790558a092784454102433beb2c6222
Anusha <66325804+anushacodes@users.noreply.github.com>
2026-09-03 14:11:51 -0500

    92 ms (3.53%) 19.7 MB (12.88%) Easy

---
+++ 0682-baseball-game/0682-baseball-game-09-03-2026-14-11-50.py
@@ -0,0 +1,37 @@
+class Solution:
+    def calPoints(self, ops: List[str]) -> int:
+        # D -> double prev score
+        # C -> remove last score
+        # + -> sum prev 2 scores
+
+        st = []
+
+        for op in ops:
+            if op.isnumeric() == True or op[0]=="-" and op[1:].isnumeric():
+                print("op isnum:", op)
+                st.append(op)
+                print(st)
+
+            elif op == 'D':
+                print("op D:", op)
+                st.append(2*int(st[-1]))
+                print(st)
+
+            elif op == 'C':
+                print("op C:", op)
+                st.pop()
+                print(st)
+
+            elif op == '+':
+                print("op +:", op)
+                st.append(int(st[-1]) + int(st[-2]))
+                print(st)
+
+
+        sum = 0
+        for i in st:
+            sum += int(i)
+        return sum
+
+
+            
